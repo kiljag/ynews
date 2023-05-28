03b0e93e28f20dc20fdb2f68659a11da64c9ec62
Jagadeesh Killi <kjaga12dev@gmail.com>
2023-05-28 22:59:09 +0530

    minor fixes

--- yapi/main.go
+++ yapi/main.go
@@ -7,10 +7,6 @@ func main() {
 	top := GetMissing(FetchYTop500())
 	fmt.Println("found : ", len(top))
 
-	if len(top) > 5 {
-		top = top[:5]
-	}
-
 	for i, id := range top {
 		fmt.Println(i, "fetching ask thread ", id)
 		items := FetchYAskItems(id)
